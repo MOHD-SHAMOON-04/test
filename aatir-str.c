9c7845e62801308a74a898f98277d27407d85701
MOHD-SHAMOON-04 <mohdshamoon04@gmail.com>
2024-07-30 20:40:21 +0530

    edited aatir-str.c

--- aatir-str.c
+++ aatir-str.c
@@ -12,62 +12,6 @@ int main()
     {
         printf(",%c,\n",stud[i].marks);
     }
-    
+    //a structure is initialised with 0 for integers and null for characters
     return 0;
 }
-///////////////////////////////////////////////////////////////
-//
-/*
-while (choice != 4)
-    {
-        printf("MENU:\n1. add details\n2.print by roll\n3.topper\n4.exit\n");
-        scanf("%d", &choice);
-
-        if (choice == 1)
-        {
-            printf("enter ROLL NO. of student: ");
-            scanf("%d", &stud[index].roll_no);
-            printf("enter MARKS of student: ");
-            scanf("%d", &stud[index].marks);
-            index++;
-            prt_table(index);
-        }
-        else if (choice == 2)
-        {
-            int temp;
-            printf("enter ROLL NO. to be searched: ");
-            scanf("%d", &temp);
-            for (int i = 0; i <= index; i++)
-            {
-                if (temp == stud[i].roll_no)
-                {
-                    printf("student found\n");
-                    prt_details(i);
-                    break;
-                }
-            }
-        }
-
-        else if (choice == 3)
-        {
-            int top_ind = 0;
-            int temp = stud[0].marks;
-            for (int i = 0; i <= index; i++)
-            {
-                if (temp < stud[i].marks)
-                {
-                    temp = stud[i].marks;
-                    top_ind = i;
-                }
-            }
-            printf("student found\n");
-            prt_details(top_ind);
-        }
-        else if (choice == 4)
-        {
-            printf("exitt!");
-            break;
-        }
-    }
-
-    */
